cf62de4659bc2c479af26a527e97f8372f954b25
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-06 20:11:44 +0200

    Add many to many relationships in person model

--- db/models/person.js
+++ db/models/person.js
@@ -20,13 +20,41 @@ module.exports = (sequelize) => {
   }, { sequelize });
 
   Person.associate = (models) => {
-    // TODO Add associations.
+    /**
+     * This is the "many" side of the movie director one-to-many
+     * relationship between the Person and Movie models. Because
+     * we gave the foreign key in the Person model a custom name
+     * we need to reference our custom name here too so that
+     * Sequelize can create the proper association. If we don't
+     * do this, Sequelize won't understand the association and
+     * will add a `PersonId` column to the `People` table
+     * (the table for the `Person` model).
+     *
+     * Note: In order to control the nullability of the `directorPersonId`
+     * property both sides of the association need to have the same
+     * configuration, otherwise you could encounter unexpected results.
+     */
+
     Person.hasMany(models.Movie, {
-      as: 'director', // alias
+      as: 'director',
       foreignKey: {
         fieldName: 'directorPersonId',
-        allowNull: false
+        field: 'directorPersonId',
+        allowNull: false,
       },
+    });
+
+    /**
+     * This is the other side of the actors many-to-many relationship
+     * between the Person and Movie models. Both sides of the association
+     * are defined in the same way.
+     */
+
+    Person.belongsToMany(models.Movie, {
+      as: 'actor',
+      through: 'MovieActors',
+      foreignKey: 'personId',
+      otherKey: 'movieId',
     });
   };
 
